b28b52581dbd1326cfb8ce9fa07de5d3938b0417
1612657466daxiong <1612657466@qq.com>
2016-11-01 16:28:15 +0800

    add butternife and Resultbean

---
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/bean/Result.java
@@ -0,0 +1,41 @@
+package cn.ucai.superwechat.bean;
+
+public class Result {
+	private int retCode = -1;
+	private boolean retMsg;
+	private Object retData;
+	public Result() {
+	}
+	public Result(boolean retMsg, int retCode){
+		this.retMsg = retMsg;
+		this.retCode = retCode;
+	}
+	public Result(int retCode, boolean retMsg, Object retData) {
+		super();
+		this.retCode = retCode;
+		this.retMsg = retMsg;
+		this.retData = retData;
+	}
+	public int getRetCode() {
+		return retCode;
+	}
+	public void setRetCode(int retCode) {
+		this.retCode = retCode;
+	}
+	public boolean isRetMsg() {
+		return retMsg;
+	}
+	public void setRetMsg(boolean retMsg) {
+		this.retMsg = retMsg;
+	}
+	public Object getRetData() {
+		return retData;
+	}
+	public void setRetData(Object retData) {
+		this.retData = retData;
+	}
+	@Override
+	public String toString() {
+		return "Result [retCode=" + retCode + ", retMsg=" + retMsg + ", retData=" + retData + "]";
+	}
+}
--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ChoseActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/ChoseActivity.java
@@ -1,26 +1,31 @@
 package cn.ucai.superwechat.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
 import cn.ucai.superwechat.utils.MFGT;
 
 public class ChoseActivity extends AppCompatActivity {
-    Button btnlogin,btnregister;
+    Button btnlogin, btnregister;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chose);
-        btnlogin= (Button) findViewById(R.id.chose_login);
-        btnregister= (Button) findViewById(R.id.chose_sign_up);
+        ButterKnife.inject(this);
+        btnlogin = (Button) findViewById(R.id.chose_login);
+        btnregister = (Button) findViewById(R.id.chose_sign_up);
 
     }
 
     public void onclick(View view) {
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.chose_login:
                 MFGT.gotoLoginActivity(this);
                 break;
