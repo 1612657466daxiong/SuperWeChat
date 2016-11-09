7c22ed563a0fe24dc7752ec3569fda70846a76f8
1612657466daxiong <1612657466@qq.ocm>
2016-11-09 19:27:41 +0800

    load user avater picture and nick in chatfragment

--- easeui/src/com/hyphenate/easeui/widget/chatrow/EaseChatRow.java
+++ easeui/src/com/hyphenate/easeui/widget/chatrow/EaseChatRow.java
@@ -116,10 +116,10 @@ public abstract class EaseChatRow extends LinearLayout {
         }
         //set nickname and avatar
         if(message.direct() == Direct.SEND){
-            EaseUserUtils.setUserAvatar(context, EMClient.getInstance().getCurrentUser(), userAvatarView);
+            EaseUserUtils.setAppUserAvatar(context, EMClient.getInstance().getCurrentUser(),userAvatarView);
         }else{
-            EaseUserUtils.setUserAvatar(context, message.getFrom(), userAvatarView);
-            EaseUserUtils.setUserNick(message.getFrom(), usernickView);
+            EaseUserUtils.setAppUserAvatar(context, message.getFrom(), userAvatarView);
+            EaseUserUtils.setAppUserNick(message.getFrom(), usernickView);
         }
         
         if(deliveredView != null){
