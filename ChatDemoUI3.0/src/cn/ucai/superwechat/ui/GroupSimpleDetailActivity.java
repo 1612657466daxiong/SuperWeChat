362ffa1f5dca2f5d5c529e6a7379935bbdbd1e1e
1612657466daxiong <1612657466@qq.ocm>
2016-11-12 14:47:37 +0800

    change groupsimpledetail title style black

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GroupSimpleDetailActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GroupSimpleDetailActivity.java
@@ -1,10 +1,10 @@
 /**
  * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
- *
+ * <p/>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -26,135 +26,161 @@ import android.widget.Toast;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMGroup;
 import com.hyphenate.chat.EMGroupInfo;
-import cn.ucai.superwechat.R;
-
 import com.hyphenate.easeui.utils.EaseUserUtils;
 import com.hyphenate.exceptions.HyphenateException;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+import cn.ucai.superwechat.R;
+
 public class GroupSimpleDetailActivity extends BaseActivity {
-	private Button btn_add_group;
-	private TextView tv_admin;
-	private TextView tv_name;
-	private TextView tv_introduction;
-	private EMGroup group;
-	private String groupid;
-	private ProgressBar progressBar;
-	private ImageView mivavatar;
+    @InjectView(R.id.iv_back)
+    ImageView mivBack;
+    @InjectView(R.id.tv_title)
+    TextView mtvTitle;
+    @InjectView(R.id.avatar)
+    ImageView mivavatar;
+    @InjectView(R.id.name)
+    TextView tv_name;
+    @InjectView(R.id.tv_admin)
+    TextView tv_admin;
+    @InjectView(R.id.tv_introduction)
+    TextView tv_introduction;
+    @InjectView(R.id.btn_add_to_group)
+    Button btn_add_group;
+    @InjectView(R.id.loading)
+    ProgressBar progressBar;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.em_activity_group_simle_details);
-		tv_name = (TextView) findViewById(R.id.name);
-		tv_admin = (TextView) findViewById(R.id.tv_admin);
-		btn_add_group = (Button) findViewById(R.id.btn_add_to_group);
-		tv_introduction = (TextView) findViewById(R.id.tv_introduction);
-		progressBar = (ProgressBar) findViewById(R.id.loading);
-		mivavatar= (ImageView) findViewById(R.id.avatar);
 
-		EMGroupInfo groupInfo = (EMGroupInfo) getIntent().getSerializableExtra("groupinfo");
-		String groupname = null;
-		if(groupInfo != null){
-		    groupname = groupInfo.getGroupName();
-		    groupid = groupInfo.getGroupId();
-		}else{
-		    group = PublicGroupsSeachActivity.searchedGroup;
-		    if(group == null)
-		        return;
-		    groupname = group.getGroupName();
-		    groupid = group.getGroupId();
-		}
-		
-		tv_name.setText(groupname);
-		
-		
-		if(group != null){
-		    showGroupDetail();
-		    return;
-		}
-		new Thread(new Runnable() {
+    private EMGroup group;
+    private String groupid;
 
-			public void run() {
-				//get detail from server
-				try {
-					group = EMClient.getInstance().groupManager().getGroupFromServer(groupid);
-					runOnUiThread(new Runnable() {
-						public void run() {
-							showGroupDetail();
-						}
-					});
-				} catch (final HyphenateException e) {
-					e.printStackTrace();
-					final String st1 = getResources().getString(R.string.Failed_to_get_group_chat_information);
-					runOnUiThread(new Runnable() {
-						public void run() {
-							progressBar.setVisibility(View.INVISIBLE);
-							Toast.makeText(GroupSimpleDetailActivity.this, st1+e.getMessage(), Toast.LENGTH_LONG).show();
-						}
-					});
-				}
-				
-			}
-		}).start();
-		
-	}
-	
-	//join the group
-	public void addToGroup(View view){
-		String st1 = getResources().getString(R.string.Is_sending_a_request);
-		final String st2 = getResources().getString(R.string.Request_to_join);
-		final String st3 = getResources().getString(R.string.send_the_request_is);
-		final String st4 = getResources().getString(R.string.Join_the_group_chat);
-		final String st5 = getResources().getString(R.string.Failed_to_join_the_group_chat);
-		final ProgressDialog pd = new ProgressDialog(this);
-		pd.setMessage(st1);
-		pd.setCanceledOnTouchOutside(false);
-		pd.show();
-		new Thread(new Runnable() {
-			public void run() {
-				try {
-					//if group is membersOnly，you need apply to join
-					if(group.isMembersOnly()){
-					    EMClient.getInstance().groupManager().applyJoinToGroup(groupid, st2);
-					}else{
-					    EMClient.getInstance().groupManager().joinGroup(groupid);
-					}
-					runOnUiThread(new Runnable() {
-						public void run() {
-							pd.dismiss();
-							if(group.isMembersOnly())
-								Toast.makeText(GroupSimpleDetailActivity.this, st3, Toast.LENGTH_SHORT).show();
-							else
-								Toast.makeText(GroupSimpleDetailActivity.this, st4, Toast.LENGTH_SHORT).show();
-							btn_add_group.setEnabled(false);
-						}
-					});
-				} catch (final HyphenateException e) {
-					e.printStackTrace();
-					runOnUiThread(new Runnable() {
-						public void run() {
-							pd.dismiss();
-							Toast.makeText(GroupSimpleDetailActivity.this, st5+e.getMessage(), Toast.LENGTH_SHORT).show();
-						}
-					});
-				}
-			}
-		}).start();
-	}
-	
-     private void showGroupDetail() {
-         progressBar.setVisibility(View.INVISIBLE);
 
-         //get group detail, and you are not in, then show join button
-         if(!group.getMembers().contains(EMClient.getInstance().getCurrentUser()))
-             btn_add_group.setEnabled(true);
-         tv_name.setText(group.getGroupName());
-         tv_admin.setText(group.getOwner());
-         tv_introduction.setText(group.getDescription());
-		 EaseUserUtils.setAppGroupAvatar(this,groupid,mivavatar);
-     }
-	
-	public void back(View view){
-		finish();
-	}
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.em_activity_group_simle_details);
+        ButterKnife.inject(this);
+        initView();
+
+
+
+        EMGroupInfo groupInfo = (EMGroupInfo) getIntent().getSerializableExtra("groupinfo");
+        String groupname = null;
+        if (groupInfo != null) {
+            groupname = groupInfo.getGroupName();
+            groupid = groupInfo.getGroupId();
+        } else {
+            group = PublicGroupsSeachActivity.searchedGroup;
+            if (group == null)
+                return;
+            groupname = group.getGroupName();
+            groupid = group.getGroupId();
+        }
+
+        tv_name.setText(groupname);
+
+
+        if (group != null) {
+            showGroupDetail();
+            return;
+        }
+        new Thread(new Runnable() {
+
+            public void run() {
+                //get detail from server
+                try {
+                    group = EMClient.getInstance().groupManager().getGroupFromServer(groupid);
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            showGroupDetail();
+                        }
+                    });
+                } catch (final HyphenateException e) {
+                    e.printStackTrace();
+                    final String st1 = getResources().getString(R.string.Failed_to_get_group_chat_information);
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            progressBar.setVisibility(View.INVISIBLE);
+                            Toast.makeText(GroupSimpleDetailActivity.this, st1 + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+
+            }
+        }).start();
+
+    }
+
+    private void initView() {
+        mivBack.setVisibility(View.VISIBLE);
+        mtvTitle.setVisibility(View.VISIBLE);
+        mtvTitle.setText(getString(R.string.group_simple_detail));
+    }
+
+    //join the group
+    public void addToGroup(View view) {
+        String st1 = getResources().getString(R.string.Is_sending_a_request);
+        final String st2 = getResources().getString(R.string.Request_to_join);
+        final String st3 = getResources().getString(R.string.send_the_request_is);
+        final String st4 = getResources().getString(R.string.Join_the_group_chat);
+        final String st5 = getResources().getString(R.string.Failed_to_join_the_group_chat);
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setMessage(st1);
+        pd.setCanceledOnTouchOutside(false);
+        pd.show();
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    //if group is membersOnly，you need apply to join
+                    if (group.isMembersOnly()) {
+                        EMClient.getInstance().groupManager().applyJoinToGroup(groupid, st2);
+                    } else {
+                        EMClient.getInstance().groupManager().joinGroup(groupid);
+                    }
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            pd.dismiss();
+                            if (group.isMembersOnly())
+                                Toast.makeText(GroupSimpleDetailActivity.this, st3, Toast.LENGTH_SHORT).show();
+                            else
+                                Toast.makeText(GroupSimpleDetailActivity.this, st4, Toast.LENGTH_SHORT).show();
+                            btn_add_group.setEnabled(false);
+                        }
+                    });
+                } catch (final HyphenateException e) {
+                    e.printStackTrace();
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            pd.dismiss();
+                            Toast.makeText(GroupSimpleDetailActivity.this, st5 + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    private void showGroupDetail() {
+        progressBar.setVisibility(View.INVISIBLE);
+
+        //get group detail, and you are not in, then show join button
+        if (!group.getMembers().contains(EMClient.getInstance().getCurrentUser()))
+            btn_add_group.setEnabled(true);
+        tv_name.setText(group.getGroupName());
+        tv_admin.setText(group.getOwner());
+        tv_introduction.setText(group.getDescription());
+        EaseUserUtils.setAppGroupAvatar(this, groupid, mivavatar);
+    }
+
+    public void back(View view) {
+        finish();
+    }
+
+    @OnClick(R.id.iv_back)
+    public void onClick() {
+        finish();
+    }
 }
